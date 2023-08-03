c907b359d809a189b400d3321453593256fc6b0c
faja rivaldi chan <fajarrivaldi2015@gmail.com>
2023-08-03 15:54:01 +0700

    add routes

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const dotenv = require("dotenv")
+
+// import routes
+const Routes = require("./app/routes")
 
 const app = express();
+
+//dotenv (.env) config
+dotenv.config()
+
+
+
+// //routes middleware
+app.use("/api", Routes)
 
 app.listen(4000, () => {
   console.log('Server is running at http://localhost:4000');
