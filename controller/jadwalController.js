8c6c627311662a848d56f6b4d4f445cf2504ae43
faja rivaldi chan <fajarrivaldi2015@gmail.com>
2023-08-10 15:04:35 +0700

    fix: perubahan message respons jadwal mu

--- controller/jadwalController.js
+++ controller/jadwalController.js
@@ -2,10 +2,20 @@ const getJadwal = (db) => async (req, res) => {
   try {
     const query = 'SELECT  udd.nama as nama, kegiatan.instansi as instansi, kegiatan.alamat as alamat, kegiatan.jumlah as jumlah,kegiatan.lat as lat,kegiatan.lng as lng  FROM kegiatan JOIN udd ON kegiatan.udd = ayodonor.udd.id WHERE DATE_FORMAT(kegiatan.TglPenjadwalan, "%Y-%d-%m") = DATE_FORMAT(CURDATE(), "%Y-%d-%m") ORDER BY udd.id';
     const [results] = await db.promise().query(query);
-    res.json(results);
+    res.status(200).json({
+      response: 200,
+      success: true,
+      message: 'fetching results successfully',
+      data: results 
+    })
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Failed to retrieve users' });
+    res.status(500).json({
+      response: 500,
+      success: true,
+      message: 'error processing',
+      data: []
+    });
   }
 }
 
@@ -14,10 +24,20 @@ const searchInstansi = (db) => async (req, res) => {
     const searchKeyword = req.query.keyword || '';
     const query = 'SELECT  udd.nama as nama, kegiatan.instansi as instansi, kegiatan.alamat as alamat, kegiatan.jumlah as jumlah,kegiatan.lat as lat,kegiatan.lng as lng  FROM kegiatan JOIN udd ON kegiatan.udd = ayodonor.udd.id WHERE DATE_FORMAT(kegiatan.TglPenjadwalan, "%Y-%d-%m") = DATE_FORMAT(CURDATE(), "%Y-%d-%m") AND instansi LIKE ? ORDER BY udd.id';
     const [results] = await db.promise().query(query, [`%${searchKeyword}%`]);
-    res.json(results);
+    res.status(200).json({
+      response: 200,
+      success: true,
+      message: 'fetching results successfully',
+      data: results 
+    })
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Failed to search for users' });
+    res.status(500).json({
+      response: 500,
+      success: true,
+      message: 'error processing',
+      data: []
+    });
   }
 };
 
