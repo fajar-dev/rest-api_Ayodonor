edcf5d898bd0c09dd8f0a2784d957e213267443b
faja rivaldi chan <fajarrivaldi2015@gmail.com>
2023-08-10 15:01:39 +0700

    fix: perubahan response massage berita

--- controller/beritaController.js
+++ controller/beritaController.js
@@ -2,10 +2,20 @@ const getBerita = (db) => async (req, res) => {
   try {
     const query = 'SELECT tb_berita.id, tb_berita.gambar as foto, tb_berita.judul as judul_berita, DATE_FORMAT(tb_berita.tgl,"%d %b %y")as tanggal_posting, tb_berita.penulis, udd.nama FROM tb_berita JOIN udd ON tb_berita.udd = udd.id ORDER BY tb_berita.tgl DESC limit 20';
     const [results] = await db.promise().query(query);
-    res.json(results);
+    res.status(200).json({
+      response: 200,
+      success: true,
+      message: 'fetching results successfully',
+      data: results 
+    })
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Failed to retrieve users' });
+    res.status(500).json({
+      response: 500,
+      success: true,
+      message: 'error processing',
+      data: []
+    });
   }
 }
 
@@ -15,13 +25,28 @@ const getBeritaById = (db) => async (req, res) => {
     const query = 'SELECT tb_berita.id, tb_berita.gambar as foto, tb_berita.judul as judul_berita, DATE_FORMAT(tb_berita.tgl,"%d %b %y")as tanggal_posting, tb_berita.berita as isi_berita, tb_berita.penulis, udd.nama FROM tb_berita JOIN udd ON tb_berita.udd = udd.id WHERE tb_berita.id = ? ORDER BY tb_berita.tgl DESC limit 20';
     const [results] = await db.promise().query(query, [id]);
     if (results.length === 0) {
-      res.status(404).json({ error: 'User not found' });
+      res.status(204).json({
+        response: 204,
+        success: true,
+        message: 'results not found',
+        data: []
+      })    
     } else {
-      res.json(results[0]);
+      res.status(200).json({
+        response: 200,
+        success: true,
+        message: 'fetching results successfully',
+        data: results[0]
+      })    
     }
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Failed to retrieve users' });
+    res.status(500).json({
+      response: 500,
+      success: true,
+      message: 'error processing',
+      data: []
+    });
   }
 }
 
